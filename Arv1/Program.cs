6a6186c80e65f4aedc461d3eee6d6c0f1c1e16a0
Mads Bjerke-Jørgensen <mjoe@hth.no>
2018-02-06 14:44:05 +0100

    Dag 4

---
+++ Arv1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arv1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person() { Efternavn = "x", Fornavn = "y" };
+            Elev e = new Elev() { Efternavn = "t", Fornavn = "g", KlasseLokale = "A" };
+            Instruktør i = new Instruktør() { Efternavn = "2", Fornavn = "s", NøgleId = 1 };
+            Console.WriteLine(p.FuldtNavn());
+            Console.WriteLine(e.FuldtNavn());
+            Console.WriteLine(i.FuldtNavn());
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                System.Console.Write("Press any key to continue . . . ");
+                System.Console.ReadKey();
+            }
+        }
+    }
+
+    public class Person
+    {
+
+        public string Fornavn { get; set; }
+        public string Efternavn { get; set; }
+        
+        public string FuldtNavn()
+        {
+            return $"{Fornavn} {Efternavn}";
+        }
+
+    }
+
+    public class Elev : Person
+    {
+        public string KlasseLokale { get; set; }
+    }
+
+    public class Instruktør : Person
+    {
+        public int NøgleId { get; set; }
+
+
+    }
+
+
+
+
+
+
+}
---
+++ Arv2Random/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arv2Random
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UdvidetRandom r = new UdvidetRandom();
+            Console.WriteLine(r.NextBool());
+
+
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                System.Console.Write("Press any key to continue . . . ");
+                System.Console.ReadKey();
+            }
+        }
+    class UdvidetRandom : System.Random
+        {
+            public bool NextBool()
+            {
+                return this.Next(1, 1002) < 500;
+            }
+        }
+    }
+
+}
